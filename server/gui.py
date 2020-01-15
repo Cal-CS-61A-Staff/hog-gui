d22dc3bafd507b6c675873457fbbdf8ea642919f
rahularya <rahularya@berkeley.edu>
2020-01-15 12:17:39 -0800

    various changes

--- server/gui.py
+++ server/gui.py
@@ -1,4 +1,6 @@
 """Web server for the hog GUI."""
+import io
+from contextlib import redirect_stdout
 
 from gui_files.common_server import route, start
 
@@ -16,21 +18,35 @@ class HogLoggingException(Exception):
 
 
 @route
-def take_turn(scores, num_rolls):
+def take_turn(scores, num_rolls, prev_rolls):
     fair_dice = dice.make_fair_dice(6)
     dice_results = []
 
     def logged_dice():
-        out = fair_dice()
+        if len(dice_results) < len(prev_rolls):
+            out = prev_rolls[len(dice_results)]
+        else:
+            out = fair_dice()
         dice_results.append(out)
         return out
 
     final_scores = None
+    final_message = None
+
+    commentary = hog.both(
+        hog.announce_highest(0),
+        hog.both(hog.announce_highest(1), hog.announce_lead_changes()),
+    )
 
     def log(*logged_scores):
-        nonlocal final_scores
+        nonlocal final_scores, final_message
         final_scores = logged_scores
-        raise HogLoggingException()
+        f = io.StringIO()
+        with redirect_stdout(f):
+            commentary(*logged_scores)
+        final_message = f.getvalue()
+        if len(dice_results) == len(prev_rolls) + num_rolls:
+            raise HogLoggingException()
 
     strategy0 = lambda s1, s2: num_rolls
     strategy1 = None  # should never be called
@@ -41,10 +57,7 @@ def take_turn(scores, num_rolls):
     except HogLoggingException:
         pass
 
-    return {
-        "rolls": dice_results,
-        "finalScores": final_scores
-    }
+    return {"rolls": dice_results, "finalScores": final_scores, "message": final_message}
 
 
 app = start(PORT, DEFAULT_SERVER, GUI_FOLDER)
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // @flow
 import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -35,15 +35,19 @@ export default function App() {
     const [scores, setScores] = useState<[number, number]>([0, 0]);
     const [numRolls, setNumRolls] = useState<number>(0);
 
+    const rollHistory = useRef([]);
+
     const handleRoll = async (inputNumRolls) => {
         setState(states.ROLLING_DICE);
         setNumRolls(inputNumRolls);
         const orderedScores = [scores[playerIndex], scores[1 - playerIndex]];
-        const [{ rolls, finalScores }] = await Promise.all([
-            post("/take_turn", { scores: orderedScores, numRolls: inputNumRolls }),
+        const [{ message, rolls, finalScores }] = await Promise.all([
+            post("/take_turn", { scores: orderedScores, numRolls: inputNumRolls, prevRolls: rollHistory.current }),
             wait(3000),
         ]);
-        setDisplayedRolls(rolls);
+        console.log(message);
+        rollHistory.current = rolls;
+        setDisplayedRolls(rolls.slice(rollHistory.current.length));
         setScores([finalScores[playerIndex], finalScores[1 - playerIndex]]);
         setState(states.DISPLAYING_CHANGE);
         await wait(1000);
