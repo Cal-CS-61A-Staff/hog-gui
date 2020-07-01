242d8e3c83a1c27652a8d82d106431f3cf2fb8da
Nancy <nshaw99@berkeley.edu>
2020-07-01 15:40:37 -0700

    updated for summer

---
+++ server/gui_files/precache-manifest.77206228a3dbca1333a8e5030c82104d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d724dab370baf26f71cd4c49cc7476f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "716f19a8f93db46086bf",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "460547915379b5d3f652",
+    "url": "/static/css/main.dfa42325.chunk.css"
+  },
+  {
+    "revision": "716f19a8f93db46086bf",
+    "url": "/static/js/2.36722391.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.36722391.chunk.js.LICENSE"
+  },
+  {
+    "revision": "460547915379b5d3f652",
+    "url": "/static/js/main.0cbd8667.chunk.js"
+  },
+  {
+    "revision": "5a695eb2126470026fed",
+    "url": "/static/js/runtime-main.71e877b4.js"
+  }
+]);
